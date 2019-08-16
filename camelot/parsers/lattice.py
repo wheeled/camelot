12a8919594f04f97c1e79d061adbbd70ddde5c0f
wheeled <dwheeler1512@yahoo.com.au>
2019-08-16 14:31:00 +1000

    Correct bugs in first pass hybrid code

--- camelot/parsers/lattice.py
+++ camelot/parsers/lattice.py
@@ -300,28 +300,9 @@ class Lattice(BaseParser):
             table_bbox, vertical_segments, horizontal_segments, pdf_scalers
         )
 
-    def _generate_columns_and_rows(self, table_idx, tk):
+    def _generate_columns_and_rows(self, table_idx, tk, horizontal_segments=None, vertical_segments=None):
         # select elements which lie within table_bbox
-
         v_s, h_s = self.select_table_bbox_elements(tk)
-
-        # t_bbox = {}
-        #
-        # v_s, h_s = segments_in_bbox(
-        #     tk,
-        #     self.vertical_segments,
-        #     self.horizontal_segments
-        # )
-        #
-        # t_bbox["horizontal"] = text_in_bbox(tk, self.horizontal_text)
-        # t_bbox["vertical"] = text_in_bbox(tk, self.vertical_text)
-        #
-        # t_bbox["horizontal"].sort(key=lambda x: (-x.y0, x.x0))
-        # t_bbox["vertical"].sort(key=lambda x: (x.x0, -x.y0))
-        #
-        # self.t_bbox = t_bbox
-        #
-
 
         cols, rows = zip(*self.table_bbox[tk])
         cols, rows = list(cols), list(rows)
